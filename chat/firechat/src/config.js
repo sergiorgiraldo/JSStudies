c38400b2fb709a5ae922c4a1ae459ce75373e30d
Sergio Giraldo at PagSeguro <sgiraldo@uolinc.com>
2019-05-06 14:59:23 -0300

    chat with firechat

---
+++ chat/firechat/src/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgmYXnonM6Y6vXOsd0FXgI3G1BN8egmcE",
+    authDomain: "vuefirechat-685e4.firebaseapp.com",
+    databaseURL: "https://vuefirechat-685e4.firebaseio.com",
+    projectId: "vuefirechat-685e4",
+    storageBucket: "vuefirechat-685e4.appspot.com",
+    messagingSenderId: "1053999009354",
+    appId: "1:1053999009354:web:c655ae8ed7ae16db"
+  };
+  export default firebaseConfig
---
+++ chat/firechat/src/store/conversation.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import uuidv4 from 'uuid/v4'
+const state = {
+  all: {},
+  allIds: [],
+  allMsgIds: []
+}
+const mutations = {
+    SET_CONVERSATION (state, { conversation }) {
+        const data = conversation.data()
+        state.all = {
+          ...state.all, 
+          [conversation.id]: { users: data.users, created: data.created, messages: [] }
+        } 
+        state.allIds.push(conversation.id)
+      }  
+}
+const actions = { 
+  seed ({ rootState }) {
+    let convoRef = rootState.db.collection('conversations')
+    convoRef.add({
+      created: Date.now(),
+      users: ['mr_a', 'mr_b'],
+      messages: [
+        { id: uuidv4(), text: 'Hi there', sender: 'mr_a', created: Date.now() },
+        { id: uuidv4(), text: 'Hi to you too!', sender: 'mr_b', created: Date.now() }
+      ]
+    })
+    convoRef.add({
+      created: Date.now(),
+      users: ['mr_a', 'mr_c'],
+      messages: []
+    })
+  },
+  async get ({ commit, rootState }) {
+    let convoRef = rootState.db.collection('conversations')
+    let convos = await convoRef.get()
+    convos.forEach(conversation => commit('SET_CONVERSATION', { conversation }))
+  }
+}
+export default { namespaced: true, state, mutations, actions }
