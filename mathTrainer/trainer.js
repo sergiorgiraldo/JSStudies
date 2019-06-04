76e8a974cfe7bac17fa94f0963d33832675ae81a
Sergio Giraldo <sergiorgiraldo@gmail.com>
2019-06-04 09:22:27 -0300

    refactoring

--- mathTrainer/trainer.js
+++ mathTrainer/trainer.js
@@ -59,7 +59,8 @@
      * @returns {Number} Random int in [min, max]
      */
     var randomInt = function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+      var result = Math.floor(Math.random() * (max - min + 1)) + min;
+      return result;
     };
 
     /**
