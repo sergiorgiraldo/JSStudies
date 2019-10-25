af939173150e94327d1ca5ec4edb225598b8a18c
Sergio Giraldo <sergio@giraldo.com.br>
2019-10-25 10:32:26 -0300

    Updates by sgiraldo @ 20191025 10:32 (using browserify to use node modules in heml)

--- functionalProgramming/myindex.js
+++ functionalProgramming/myindex.js
@@ -1,9 +1,22 @@
 //npm install -g browserify
+const R = require('ramda');
 
-var jsonData = '{"persons":[{"name":"John","city":"New York"},{"name":"Phil","city":"Ohio"}]}';
- 
-var jsonParsed = JSON.parse(jsonData);
- 
-function showFirstCity() {
-    return jsonParsed.persons[0].name;
-}
+const cities = require('./cities.json');
+
+const KelvinToCelsius = k => {
+    return k - 273.15;
+};
+
+const updateTemperature = city => {
+    const temp = Math.round(KelvinToCelsius(city.temp));
+    return R.merge(city, {temp});
+};
+
+const updatedCities = cities.map(updateTemperature);
+
+window.Cities = function () {
+    return JSON.stringify(cities);
+};
+window.UpdatedCities = function () {
+    return JSON.stringify(updatedCities);
+};
