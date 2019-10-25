f1c3ad8121b8bbccdedfb017efebe1aee7ab6c4a
Sergio Giraldo <sergio@giraldo.com.br>
2019-10-25 17:02:03 -0300

    Updates by sgiraldo @ 20191025 17:02 (curry and reduce)

--- functionalProgramming/bundle.js
+++ functionalProgramming/bundle.js
@@ -29,10 +29,41 @@ module.exports=[
     "temp": 301.48333333333335,
     "humidity": 88,
     "internetSpeed": 20
+  },
+  {
+    "name": "Phuket",
+    "country": "TH",
+    "region": "SE Asia",
+    "cost": 1040,
+    "currency": "USD",
+    "temp": 302.03888888888895,
+    "humidity": 74,
+    "internetSpeed": 20
+  },
+  {
+    "name": "Hoi An",
+    "country": "VN",
+    "region": "SE Asia",
+    "cost": 1083,
+    "currency": "USD",
+    "temp": 299.2611111111112,
+    "humidity": 83,
+    "internetSpeed": 20
+  },
+  {
+    "name": "Prague",
+    "country": "CZ",
+    "region": "Europe",
+    "cost": 1191,
+    "currency": "USD",
+    "temp": 292.03888888888895,
+    "humidity": 77,
+    "internetSpeed": 40
   }
  ]
 },{}],2:[function(require,module,exports){
 //npm install -g browserify
+//npm install -g ramda
 const R = require('ramda');
 
 const cities = require('./cities.json');
@@ -40,19 +71,47 @@ const cities = require('./cities.json');
 const KelvinToCelsius = k => {
     return k - 273.15;
 };
+const KelvinToFahrenheit = k => {
+    return k * 9 / 5 - 459.67;
+}
 
-const updateTemperature = city => {
-    const temp = Math.round(KelvinToCelsius(city.temp));
-    return R.merge(city, {temp});
+const updateTemperature = (convertFn) => {
+    return city => {
+        const temp = Math.round(convertFn(city.temp));
+        return R.merge(city, { temp });
+    }    
 };
 
-const updatedCities = cities.map(updateTemperature);
+const updateTemperature2 = R.curry((convertFn, city) => {
+    const temp = Math.round(convertFn(city.temp));
+    return R.merge(city, { temp });
+});
+  
+const updatedCities = cities.map(updateTemperature(KelvinToCelsius));
+const updatedCities2 = R.map(updateTemperature2(KelvinToCelsius), cities);
+
+const totalCostReducer = (acc, city) => {
+    const { cost = 0 } = city;
+    return acc + cost;
+}
+
+const totalCost = updatedCities2.reduce(totalCostReducer, 0);
+const totalCost2 = R.reduce(totalCostReducer, 0, updatedCities2);
 
 window.Cities = function () {
     return JSON.stringify(cities);
 };
 window.UpdatedCities = function () {
     return JSON.stringify(updatedCities);
+};
+window.UpdatedCities2 = function () {
+    return JSON.stringify(updatedCities2);
+};
+window.TotalCost = function () {
+    return totalCost;
+};
+window.TotalCost2 = function () {
+    return totalCost2;
 };
 },{"./cities.json":1,"ramda":87}],3:[function(require,module,exports){
 var always = /*#__PURE__*/require('./always');
--- functionalProgramming/myindex.js
+++ functionalProgramming/myindex.js
@@ -1,4 +1,5 @@
 //npm install -g browserify
+//npm install -g ramda
 const R = require('ramda');
 
 const cities = require('./cities.json');
@@ -6,17 +7,45 @@ const cities = require('./cities.json');
 const KelvinToCelsius = k => {
     return k - 273.15;
 };
+const KelvinToFahrenheit = k => {
+    return k * 9 / 5 - 459.67;
+}
 
-const updateTemperature = city => {
-    const temp = Math.round(KelvinToCelsius(city.temp));
-    return R.merge(city, {temp});
+const updateTemperature = (convertFn) => {
+    return city => {
+        const temp = Math.round(convertFn(city.temp));
+        return R.merge(city, { temp });
+    }    
 };
 
-const updatedCities = cities.map(updateTemperature);
+const updateTemperature2 = R.curry((convertFn, city) => {
+    const temp = Math.round(convertFn(city.temp));
+    return R.merge(city, { temp });
+});
+  
+const updatedCities = cities.map(updateTemperature(KelvinToCelsius));
+const updatedCities2 = R.map(updateTemperature2(KelvinToCelsius), cities);
+
+const totalCostReducer = (acc, city) => {
+    const { cost = 0 } = city;
+    return acc + cost;
+}
+
+const totalCost = updatedCities2.reduce(totalCostReducer, 0);
+const totalCost2 = R.reduce(totalCostReducer, 0, updatedCities2);
 
 window.Cities = function () {
     return JSON.stringify(cities);
 };
 window.UpdatedCities = function () {
     return JSON.stringify(updatedCities);
+};
+window.UpdatedCities2 = function () {
+    return JSON.stringify(updatedCities2);
+};
+window.TotalCost = function () {
+    return totalCost;
+};
+window.TotalCost2 = function () {
+    return totalCost2;
 };
