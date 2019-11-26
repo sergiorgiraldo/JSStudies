4b0dabe07779a91e76bb3c1aea2f3e8bdfc19c23
Sergio Giraldo <sergio@giraldo.com.br>
2019-11-26 14:43:00 -0300

    Updates by sgiraldo @ 20191126 14:42 (last update)

--- functionalProgramming/myindex.js
+++ functionalProgramming/myindex.js
@@ -1,66 +1,112 @@
 //npm install -g browserify
 //npm install -g ramda
 const R = require('ramda');
+const table = require('text-table');
 const cities = require('./cities.json');
 const percentile = require('./percentile');
 
-const KelvinToCelsius = k => {
-    return k - 273.15;
-};
-const KelvinToFahrenheit = k => {
-    return k * 9 / 5 - 459.67;
-}
+const KtoC = k => k - 273.15;
+const KtoF = k => k * 9 / 5 - 459.67;
 
-const updateTemperature = (convertFn) => {
-    return city => {
-        const temp = Math.round(convertFn(city.temp));
-        return R.merge(city, { temp });
-    }    
-};
-
-const updateTemperature2 = R.curry((convertFn, city) => {
-    const temp = Math.round(convertFn(city.temp));
-    return R.merge(city, { temp });
+const updateTemperature = R.curry((convertFn, city) => {
+  const temp = Math.round(convertFn(city.temp));
+  return R.merge(city, { temp });
 });
-  
-const updatedCities = cities.map(updateTemperature(KelvinToCelsius));
-const updatedCities2 = R.map(updateTemperature2(KelvinToCelsius), cities);
+
+const updatedCities = R.map(updateTemperature(KtoF), cities);
+
+// console.log(updatedCities);
+
+const city = cities[0];
+const updatedCity = updateTemperature(KtoF, city);
+// console.log(updatedCity);
 
 const totalCostReducer = (acc, city) => {
-    const { cost = 0 } = city;
-    return acc + cost;
+  const { cost = 0 } = city;
+  return acc + cost;
 }
 
-const totalCost = updatedCities2.reduce(totalCostReducer, 0);
-const totalCost2 = R.reduce(totalCostReducer, 0, updatedCities2);
-const cityCount = R.length(updatedCities2);
-const averageCost = totalCost2 / cityCount;
+const totalCost = R.reduce(totalCostReducer, 0, cities);
+const cityCount = R.length(cities);
+// console.log(totalCost / cityCount);
 
 const groupByPropReducer = (acc, city) => {
     const { cost = [], internetSpeed = [] } = acc;
     return R.merge(acc, {
-      cost: R.append(city.cost, cost),
-      internetSpeed: R.append(city.internetSpeed, internetSpeed),
+    cost: R.append(city.cost, cost),
+    internetSpeed: R.append(city.internetSpeed, internetSpeed),
     });
-  }
-  
+}
+
 const groupedByProp = R.reduce(groupByPropReducer, {}, cities);
 
+
+// console.log(groupedByProp);
+
 const calcScore = city => {
-    const { cost = 0, internetSpeed = 0 } = city;
-    const costPercentile = percentile(groupedByProp.cost, cost);
-    const internetSpeedPercentile = percentile(
-      groupedByProp.internetSpeed,
-      internetSpeed,
-    );
-    const score =
-      100 * (1.0 - costPercentile) +
-      20 * internetSpeedPercentile; 
-    return R.merge(city, { score });
-  }
-  
-  const scoredCities = R.map(calcScore, updatedCities);
-  
+  const { cost = 0, internetSpeed = 0 } = city;
+  const costPercentile = percentile(groupedByProp.cost, cost);
+  const internetSpeedPercentile = percentile(
+    groupedByProp.internetSpeed,
+    internetSpeed,
+  );
+  const score =
+    100 * (1.0 - costPercentile) +
+    20 * internetSpeedPercentile; 
+  return R.merge(city, { score });
+}
+
+// const scoredCities = R.map(calcScore, updatedCities);
+
+// console.log(scoredCities);
+
+const filterByWeather = city => {
+  const { temp = 0, humidity = 0 } = city;
+  return temp > 68 && temp < 85 && humidity > 30 && humidity < 70;
+}
+
+// const filteredCities = R.filter(filterByWeather, scoredCities);
+
+// console.log(R.length(filteredCities));
+
+// const sortedCities = R.sortWith(
+//   [R.descend(city => city.score)],
+//   filteredCities,
+// );
+
+// console.log(sortedCities);
+
+// const top10 = R.take(10, sortedCities);
+
+// console.log(top10);
+// console.log(R.length(top10));
+
+const cityToArray = city => {
+  const { name, country, score, cost, temp, internetSpeed } = city;
+  return [name, country, score, cost, temp, internetSpeed];
+};
+const interestingProps = [
+  'Name',
+  'Country',
+  'Score',
+  'Cost',
+  'Temp',
+  'Internet',
+];
+
+const topCities = R.pipe(
+  R.map(updateTemperature(KtoF)),
+  R.filter(filterByWeather),
+  R.map(calcScore),
+  R.sortWith([R.descend(city => city.score)]),
+  R.take(10),
+  R.map(cityToArray),
+  R.prepend(interestingProps),
+  table,
+)(cities);
+
+console.log(topCities);
+
 if (typeof window !== 'undefined') {
     window.Cities = function () {
         return JSON.stringify(cities);
@@ -68,16 +114,9 @@ if (typeof window !== 'undefined') {
     window.UpdatedCities = function () {
         return JSON.stringify(updatedCities);
     };
-    window.UpdatedCities2 = function () {
-        return JSON.stringify(updatedCities2);
-    };
     window.TotalCost = function () {
         return totalCost;
     };
-    window.TotalCost2 = function () {
-        return totalCost2;
-    };
-    
     window.Test = function () {
         return "Success!!!";
     };    
