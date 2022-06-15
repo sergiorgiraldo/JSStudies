33e8350d9f1668edac7baff5a38aa6c83f9a834e
Sergio Giraldo <sergio@giraldo.com.br>
2022-06-15 17:09:47 +0200

    react second day, stuck \_(ツ)_/¯

---
+++ react/a03/index.js
@@ -0,0 +1,5 @@
+// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+var h1 = document.createElement( "h1");
+h1.textContent = "Hello, React!"
+var root = document.getElementById("root");
+root.appendChild(h1);
