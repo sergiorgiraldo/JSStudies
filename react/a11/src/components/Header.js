9fc36a38c79960b9b3f48a6766aabca09aa25577
Sergio Giraldo <sergio@giraldo.com.br>
2022-06-17 22:18:20 +0200

    airbnb clone

---
+++ react/a11/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Nav from 'react-bootstrap/Nav';
+import ReactLogo from '../images/logo192.png';
+
+function Header() {
+    return( 
+        <header>
+            <Nav variant="tabs" defaultActiveKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home"><img src={ReactLogo} alt="logo" width={30} /></Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">Inner page</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="disabled" disabled>
+                        Disabled
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ react2/airbnb_01/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/header"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+function App() {
+    return (
+        <Header />
+    )
+}
+
+export default App;
