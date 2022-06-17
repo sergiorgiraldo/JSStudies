411f258e1da2a8cee3eba598ae0e1996fc1295fb
Sergio Giraldo <sergio@giraldo.com.br>
2022-06-17 16:30:55 +0200

    ignore build folder

--- react/a10/src/index.js
+++ react/a10/src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from 'react-dom';
+
 import Footer from "./components/Footer";
 import MainContent  from "./components/MainContent";
 import Header from './components/Header';
