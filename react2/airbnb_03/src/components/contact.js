efc66568e3b5791449e065758a47cabed68018f1
Sergio Giraldo <sergio@giraldo.com.br>
2022-06-19 18:56:06 +0200

    props deconstructed, props non-string

---
+++ react2/airbnb_03/src/components/contact.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Contact({img, name, phone, email}) {
+    return (
+        <div className="contact-card">
+            <img src={img}/>
+            <h3>{name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{email}</p>
+            </div>
+        </div>
+)
+}
+
+export default Contact;
