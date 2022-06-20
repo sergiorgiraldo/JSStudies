56e4c4d4de41b1843a10b68ecee966db2d05c8ae
Sergio Giraldo <sergio@giraldo.com.br>
2022-06-20 22:19:49 +0200

    using array maps

---
+++ react2/airbnb_06/src/components/joke.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Joke({setup, punchline, upvote, downvote, isPun}) {
+    return (
+        <div className="joke-card">
+            {
+            setup &&
+            <div className="info-group">
+                <p>{setup}</p>
+            </div>
+            }
+            <div className="info-group">
+                <p><b>{punchline}</b></p>
+            </div>
+            <div className="info-group">
+                <p>Other: {upvote}++, {downvote}--,{isPun?"Pun!!":"No pun :("}</p>
+            </div>
+        </div>
+)
+}
+
+export default Joke;
