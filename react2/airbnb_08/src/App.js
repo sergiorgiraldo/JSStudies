4cb67da5dbf580c0ed9f1a2f3a15f74fc8744285
Sergio Giraldo <sergio@giraldo.com.br>
2022-06-22 14:32:36 +0200

    passing objects as props

---
+++ react2/airbnb_08/src/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import NavBar from "./components/navbar"
+import Hero from "./components/hero"
+import Card from "./components/card"
+import Data from "./data.js"
+
+function App() {
+    const cards = Data.map(d=> {
+        return( 
+            <Card 
+                item={d} //if I use {..item}, then on the card, instead of props.item.whatever I would use props.whatever
+            />
+        )
+    });
+
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
+export default App;
