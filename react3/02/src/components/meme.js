53f4069033c3dee74a485cf53de27e7d470fa37f
Sergio Giraldo <sergio@giraldo.com.br>
2022-06-27 22:09:35 +0200

    meme generator first exercises

---
+++ react3/02/src/components/meme.js
@@ -0,0 +1,40 @@
+import React from "react"
+import memesData from "../memesData.js"
+
+export default function Meme() {
+    /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
+    function getRandomMeme(){
+        const total = memesData.data.memes.length;
+        const randomMeme = memesData.data.memes[Math.floor(Math.random() * total)];
+        console.log(randomMeme.url);
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getRandomMeme}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+        </main>
+    )
+}
