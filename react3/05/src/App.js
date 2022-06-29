fd59312d9ab6cdb8db4a42a96bef2f450ad877ec
Sergio Giraldo <sergio@giraldo.com.br>
2022-06-29 08:31:00 +0200

    more state on React

---
+++ react3/05/src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function App() {
+    const [count, setCount] = React.useState(0)
+
+    function handlePlusClick() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function handleMinusClick() {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={handleMinusClick}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={handlePlusClick}>+</button>
+        </div>
+    )
+}
