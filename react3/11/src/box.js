8ee43bfb70748beeec8f9a479f95fdbb5b0255de
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-02 11:48:51 +0200

    react

---
+++ react3/11/src/box.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Box(props) {
+    const styles = {
+        backgroundColor: props.box.on ? "cyan":"white"
+    }
+    return (
+        <div className="box" style={styles} onClick={props.handleClick}>
+            <span>{props.box.id}</span> 
+        </div>
+    )
+}
