e8a3968cb4f4ae2806dc5440ae02a08ab9a55fd7
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-02 19:21:38 +0200

    conditional rendering

---
+++ react3/13/src/joke.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default function Joke(props) {
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
+    const [isShown, setShown] = React.useState(false);
+
+    function toggle(){
+        setShown(prevShown => !prevShown);
+    }
+
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggle}>
+                {isShown ? "Hide" : "Show"} Punchline
+            </button>
+            <hr />
+        </div>
+    )
+}
