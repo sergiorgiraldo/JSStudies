51a2ccbcbe83d6e5357025c4e772193c36484ca2
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-19 22:07:02 +0200

    more state on React, intro to API calls:: by sergio giraldo @ 20220719T2207CEST, gpg signed

---
+++ react3/17/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./components/header"
+import Meme from "./components/meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
---
+++ react3/18/src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    
+    console.log("Component rendered")
+    
+    console.log("Component rendered")
+    
+    fetch("https://swapi.dev/api/people/1")
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+    // if we set the state inside the fetch, then React calls the function; which fetch the data, setting the state, then React calls the function, ad infinitum
+    // fetch("https://swapi.dev/api/people/1")
+    //     .then(res => res.json())
+    //     .then(data => setStarWarsData(data))
+    
+    return (
+        <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}
