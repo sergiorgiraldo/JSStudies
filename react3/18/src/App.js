2dc433327c31375a3605d4a861174e4441444c20
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-20 16:42:19 +0200

    api calls, side effects:: by sergio giraldo @ 20220720T1642CEST, gpg signed

--- react3/18/src/App.js
+++ react3/18/src/App.js
@@ -5,13 +5,11 @@ export default function App() {
     
     console.log("Component rendered")
     
-    console.log("Component rendered")
-    
     fetch("https://swapi.dev/api/people/1")
         .then(res => res.json())
         .then(data => console.log(data))
 
-    // if we set the state inside the fetch, then React calls the function; which fetch the data, setting the state, then React calls the function, ad infinitum
+    // if we set the state inside the fetch, then React calls the function; which fetch the data, setting the state, then React calls the function, which fetch the data, etc., ad infinitum
     // fetch("https://swapi.dev/api/people/1")
     //     .then(res => res.json())
     //     .then(data => setStarWarsData(data))
---
+++ react3/19/src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
+    
+    console.log("Component rendered")
+    
+    // side effects
+    React.useEffect(function() {
+        console.log("Effect ran")
+        // fetch("https://swapi.dev/api/people/1")
+        //     .then(res => res.json())
+        //     .then(data => console.log(data))
+    },[count]) //this array controls which dependencies React watch to run the side effect. If kept empty, it will run only once, s there is nothing for each to watch. 
+    
+    return (
+        <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+        </div>
+    )
+}
