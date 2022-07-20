93d6cef27891e8f169b5446db93293b7ba4aff4b
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-20 20:58:53 +0200

    setting props and event handler:: by sergio giraldo @ 20220720T2058CEST, gpg signed

---
+++ react4/01/src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function App() {
+    const [darkMode, setDarkMode] = React.useState(false);
+
+    function toggleDarkMode() {
+        setDarkMode(prevMode => {return !prevMode});
+    }
+    
+    return (
+        <div className="container">
+            <Navbar 
+                darkMode = {darkMode}
+                toggleDarkMode = {toggleDarkMode}
+            />
+            <Main 
+                darkMode = {darkMode}
+            />
+        </div>
+    )
+}
