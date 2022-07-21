e8caf0e483f7095645f3d31631f2fc6ba2251aaf
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-21 14:54:43 +0200

    notes app: local storage + first line as title in sidebar:: by sergio giraldo @ 20220721T1454CEST, gpg signed

---
+++ react4/04/src/components/Sidebar.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+export default function Sidebar(props) {
+    /**
+     * Challenge: Try to figure out a way to display only the 
+     * first line of note.body as the note summary in the
+     * sidebar.
+     * 
+     * Hint 1: note.body has "invisible" newline characters
+     * in the text every time there's a new line shown. E.g.
+     * the text in Note 1 is:
+     * "# Note summary\n\nBeginning of the note"
+     * 
+     * Hint 2: See if you can split the string into an array
+     * using the "\n" newline character as the divider
+     */
+    
+    const noteElements = props.notes.map((note, index) => (
+        <div key={note.id}>
+            <div
+                
+                className={`title ${
+                    note.id === props.currentNote.id ? "selected-note" : ""
+                }`}
+                onClick={() => props.setCurrentNoteId(note.id)}
+            >
+                <h4 className="text-snippet">
+                    {note.body == "# Type your markdown note's title here" ? `Note ${index + 1}` : note.body.split('\n')[0]}
+                </h4>
+            </div>
+        </div>
+    ))
+
+    return (
+        <section className="pane sidebar">
+            <div className="sidebar--header">
+                <h3>Notes</h3>
+                <button className="new-note" onClick={props.newNote}>+</button>
+            </div>
+            {noteElements}
+        </section>
+    )
+}
