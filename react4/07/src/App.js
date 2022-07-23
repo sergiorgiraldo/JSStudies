b189978d60d6dd56f96040893a50252b3768dd29
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-23 15:05:24 +0200

    tenzies is ready :):: by sergio giraldo @ 20220723T1505CEST, gpg signed

--- react4/07/src/App.js
+++ react4/07/src/App.js
@@ -1,8 +1,11 @@
 import React from "react"
 import Dice from "./components/Dice"
+import Confetti from "react-confetti"
 
 export default function App() {
     const [dices, setDices] = React.useState(allNewDice())
+    const [rollsNo, setRollsNo] = React.useState(0)
+    const [tenzies, setTenzies] = React.useState(false)
 
     function allNewDice(){
         const newDices =  [];
@@ -10,35 +13,74 @@ export default function App() {
         for (let index = 0; index < 10; index++) {
             newDices.push({
                 id: index,
-                value: Math.floor(Math.random() * 6) + 1, 
+                value: getRandomDiceValue(), 
                 isHeld: false
             });
         }
         return newDices;
     }
 
-    function rollDices(){
-        setDices(allNewDice());
+    function getRandomDiceValue(){
+        return Math.floor(Math.random() * 6) + 1;
     }
 
-    function toggleDice(which){
-        setDices((prevDices)=> {
-            const prevDices_ = [...prevDices];
-            const idx = prevDices_.findIndex(d => {
-                return d.id === which;
-            });
-            prevDices_[idx].isHeld = !prevDices_[idx].isHeld;    
-            return prevDices_;
+    function rollDices(){
+        if(tenzies){
+            setRollsNo(0);
+            setTenzies(false);
+            setDices(allNewDice());
+        }
+        else{
+            setDices(oldDice => oldDice.map(d => {
+                return d.isHeld ? 
+                    d :
+                    {...d, value: getRandomDiceValue()}
+            }));
+            setRollsNo(current => current + 1);
+        }
+    }
+
+    React.useEffect(_ => {
+        const allHeld = dices.every(d=>d.isHeld);
+        const allSame = dices.every(d=>d.value == dices[0].value);
+
+        setTenzies(_ => {
+            return allHeld && allSame;
         });
+    },[dices]
+    );
+
+    function toggleDice(which){
+        setDices(oldDice => oldDice.map(d => {
+            return d.id === which ? 
+                {...d, isHeld: !d.isHeld} :
+                d
+        }));
+
+        // ☝️ much more elegant
+        // setDices((prevDices)=> {
+        //     const prevDices_ = [...prevDices];
+        //     const idx = prevDices_.findIndex(d => {
+        //         return d.id === which;
+        //     });
+        //     prevDices_[idx].isHeld = !prevDices_[idx].isHeld;    
+        //     return prevDices_;
+        // });
     }
 
     return (
         <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each dice to freeze it at its current value between rolls.</p>            
+            <h2>{rollsNo} roll{rollsNo > 1?"s":""} ...</h2>
             <div className="dice-container">
                 {dices.map((d)=><Dice key={d.id} id={d.id} value={d.value} isHeld={d.isHeld} toggleDice={toggleDice}/>)}
             </div>
 
-            <button className="roll-dice" onClick={rollDices}>Roll</button>
+            <button className="roll-dice" onClick={rollDices}>
+                {tenzies ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
