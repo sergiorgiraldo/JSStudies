d7239c667586d23c18bb91983bf19847789ce120
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-24 17:46:45 +0200

    start think how to use a db hosted in docker to store stats:: by sergio giraldo @ 20220724T1746CEST, gpg signed

---
+++ react4/08/src/components/Stats.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Stats(props) {
+    const stats_ = props.stats.map((s,index) => {
+        return (
+        <ul key={index}>
+          <li>{Math.ceil(Math.round((new Date(s.endDate).getTime() - new Date(s.startDate).getTime())) / 1000)} seconds, {s.count} roll(s)</li>
+        </ul>
+        )});
+
+    return (
+        <div>
+            {stats_}
+        </div>
+    )
+}
