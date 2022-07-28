b93411241e929b1fe0084fa6b6ec24cea0b22203
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-28 22:03:42 +0200

    getting data from db:: by sergio giraldo @ 20220728T2203CEST, gpg signed

--- react4/10/src/components/Stats.js
+++ react4/10/src/components/Stats.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 export default function Stats(props) {
+
+
     const stats_ = props.stats.map((s,index) => {
         return (
         <ul key={index}>
-          <li>{Math.ceil(Math.round((new Date(s.endDate).getTime() - new Date(s.startDate).getTime())) / 1000)} seconds, {s.count} roll(s)</li>
+          <li>{s.duration} seconds, {s.count} roll(s)</li>
         </ul>
         )});
 
