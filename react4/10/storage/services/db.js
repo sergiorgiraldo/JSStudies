974aee9f2eec101fc6b4682f22bd2b819a4dc434
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-29 12:54:43 +0200

    api to save stats ready. I struggled to build it, learned that express need the body-parser to receive data in the body, whren posting:: by sergio giraldo @ 20220729T1254CEST, gpg signed

--- react4/10/storage/services/db.js
+++ react4/10/storage/services/db.js
@@ -6,7 +6,11 @@ const db = new sqlite(path.resolve('tenzie.sqlite3'), {fileMustExist: true});
 function query(sql, params) {
   return db.prepare(sql).all(params);
 }
+function run(sql, params) {
+  return db.prepare(sql).run(params);
+}
 
 module.exports = {
-  query
+  query,
+  run
 }
