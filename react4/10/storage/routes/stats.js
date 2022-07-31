1689c5ffa889b0de317ad24c19af2757de21c7e4
Sergio Giraldo <sergio@giraldo.com.br>
2022-07-31 21:31:59 +0200

    small refactorings:: by sergio giraldo @ 20220731T2131CEST, gpg signed

--- react4/10/storage/routes/stats.js
+++ react4/10/storage/routes/stats.js
@@ -8,17 +8,18 @@ express.json();
 router.get('/', function(req, res, next) {
   try {
     res.json(stats.getMultiple(req.query.page));
-  } catch(err) {
+  } 
+  catch(err) {
     console.error(`Error while getting stats `, err.message);
     next(err);
   }
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
   try {
     res.json(stats.putStat(req.body));
-  } catch(err) {
+  } 
+  catch(err) {
     console.error(`Error while adding stat `, err.message);
     next(err);
   }
