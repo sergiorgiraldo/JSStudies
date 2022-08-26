7618a2d77f912b8bf2952b515213b7e9d2121fcb
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-26 15:14:46 +0200

    Next js course:: by sergio giraldo @ 20220826T1514CEST, gpg signed

---
+++ NextJS/first/pages/index.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <h1>Hello Next World!</h1>  
+    </div>
+  )
+}
