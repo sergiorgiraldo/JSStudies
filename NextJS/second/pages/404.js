de5300b224fa1109926befeb6d35268fa9be890b
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-27 12:24:20 +0200

    more routing:: by sergio giraldo @ 20220827T1224CEST, gpg signed

---
+++ NextJS/second/pages/404.js
@@ -0,0 +1,9 @@
+function NotFoundPage(){
+
+    return( 
+    <div>
+        <h1>I dont know this one!</h1>
+    </div>
+    );
+}
+export default NotFoundPage;
--- NextJS/second/pages/clients/[id]/index.js
+++ NextJS/second/pages/clients/[id]/index.js
@@ -3,10 +3,16 @@ import { useRouter} from 'next/router'
 function ClientProjectsPage(){
     const router = useRouter();
 
+    function loadProjectHandler(){
+        //load data, etc
+        router.push(`/clients/${router.query.id}/foo`) //replace can be used and then one cant go back (navigate)
+    }
+
     return( 
     <div>
         <h1>Projects for a given client</h1>
-        <h2>Client :: {router.query.id}</h2> 
+        <h2>Client :: {router.query.id}</h2>
+        <button onClick={loadProjectHandler}>Load project Foo</button> 
     </div>
     );
 }
