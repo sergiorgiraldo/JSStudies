ec9bc3a0b526ea43a1cf3196019ddb6191303948
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-28 20:24:15 +0200

    sytles:: by sergio giraldo @ 20220828T2024CEST, gpg signed

--- NextJS/third/pages/events/[eventId].js
+++ NextJS/third/pages/events/[eventId].js
@@ -1,8 +1,25 @@
+import { useRouter} from "next/router";
+import { getEventById } from "../../dummy-data"
+import EventItem from "../../components/events/EventItem";
+
 function EventDetailPage(){
+    const router = useRouter();
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if (!event){
+        return <p>Event not found!</p>
+    }
 
     return( 
     <div>
         <h1>Event Detail</h1>
+        <EventItem 
+                    id={event.id}
+                    title={event.title}
+                    location={event.location}
+                    date={event.date}
+                    image={event.image}/>        
     </div>
     );
 }
