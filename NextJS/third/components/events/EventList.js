1da43c971c46dd9d399d6241fa99a8307e8a7459
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-28 17:10:10 +0200

    some styles, more routes, etc:: by sergio giraldo @ 20220828T1710CEST, gpg signed

---
+++ NextJS/third/components/events/EventList.js
@@ -0,0 +1,20 @@
+import EventItem from "./EventItem";
+import classes from "./EventList.module.css"
+
+function EventList(props){
+    const { events } = props;
+
+    return( 
+        <ul  className={classes.list}>
+            {events.map(e => 
+                <EventItem 
+                    id={e.id}
+                    title={e.title}
+                    location={e.location}
+                    date={e.date}
+                    image={e.image}/>)}
+        </ul> 
+    );
+}
+
+export default EventList;
---
+++ NextJS/third/pages/events/[eventId].js
@@ -0,0 +1,10 @@
+function EventDetailPage(){
+
+    return( 
+    <div>
+        <h1>Event Detail</h1>
+    </div>
+    );
+}
+
+export default EventDetailPage;
