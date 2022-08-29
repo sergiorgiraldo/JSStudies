5869659bbc0193f0599d9a9d53ecf02ad992ca32
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-29 15:49:28 +0200

    all events page plus navigation bar on shared page:: by sergio giraldo @ 20220829T1549CEST, gpg signed

--- NextJS/third/pages/events/index.js
+++ NextJS/third/pages/events/index.js
@@ -1,8 +1,12 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
+
 function AllEventsPage(){
+    const events = getAllEvents();
 
     return( 
     <div>
-        <h1>All Events page</h1>
+        <EventList events={events}></EventList>
     </div>
     );
 }
