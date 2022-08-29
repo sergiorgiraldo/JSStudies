7868da1f49d2d518a9b96e9a91f4f106bf853faa
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-29 15:49:28 +0200

    all events page plus navigation bar on shared page:: by sergio giraldo @ 20220829T1549CEST, gpg signed

---
+++ NextJS/third/components/layout/main-header.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import classes from "./main-header.module.css" 
+
+function MainHeader(){
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>
+                <Link href="/">Next Events</Link>
+            </div>
+            <nav className={classes.navigation}>
+                <ul>
+                    <li>
+                        <Link href="/events">Browse all Events</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default MainHeader;
