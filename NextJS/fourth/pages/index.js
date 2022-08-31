5e3e086edf2d06c1e3b3e4a715036f99c1734dc0
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-31 21:22:45 +0200

    static async props:: by sergio giraldo @ 20220831T2122CEST, gpg signed

--- NextJS/fourth/pages/index.js
+++ NextJS/fourth/pages/index.js
@@ -1,11 +1,25 @@
 function HomePage(props) {
+  const { products__ } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {
+        products__.map((p) => (
+          <li key={p.id}>{p.title}</li>
+        ))
+      }
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products__: [
+        { id: "P1", title: "Product 1" },
+        { id: "P2", title: "Product 2" }
+      ],
+    },
+  };
 }
 
 export default HomePage;
