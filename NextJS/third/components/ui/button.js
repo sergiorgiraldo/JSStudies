172c4a9c648c51ea60958c67dab60463ed30a254
Sergio Giraldo <sergio@giraldo.com.br>
2022-08-31 09:38:51 +0200

    file based routing:: by sergio giraldo @ 20220831T0938CEST, gpg signed

--- NextJS/third/components/ui/button.js
+++ NextJS/third/components/ui/button.js
@@ -1,19 +1,20 @@
 import Link from "next/link";
-import classes from "./Button.module.css"
+import classes from "./Button.module.css";
 
-function Button(props){
-    if (props.link){
-        return( 
-            <Link href={props.link}>
-                <a className={classes.btn}>{props.children}</a>
-            </Link>
-        );
-    }
-    else{
-        return(
-            <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
-        );
-    }
+function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  } else {
+    return (
+      <button className={classes.btn} onClick={props.onClick}>
+        {props.children}
+      </button>
+    );
+  }
 }
 
 export default Button;
--- NextJS/third/pages/events/[eventId].js
+++ NextJS/third/pages/events/[eventId].js
@@ -1,34 +1,42 @@
-import { useRouter} from "next/router";
-import { getEventById } from "../../dummy-data"
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
 import { Fragment } from "react";
-import EventSummary from "../../components/event-detail/event-summary"
-import EventLogistics from "../../components/event-detail/event-logistics"
-import EventContent from "../../components/event-detail/event-content"
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import Error from "../../components/ui/error-alert";
+import Button from "../../components/ui/button";
 
+function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
 
-function EventDetailPage(){
-    const router = useRouter();
-    const eventId = router.query.eventId;
-    const event = getEventById(eventId);
-
-    if (!event){
-        return <p>Event not found!</p>
-    }
-
-    return( 
-        <Fragment>
-            <EventSummary title={event.title}/>
-            <EventLogistics 
-                date={event.date}
-                address={event.location}
-                image={event.image}
-                imageAlt={event.title}  
-            />
-            <EventContent>
-                <p>{event.description}</p>
-            </EventContent>    
-        </Fragment> 
+  if (!event) {
+    return (
+      <Fragment>
+        <div className="center">
+          <Error>NO EVENT FOUND!</Error>
+          <Button link="/events">All events</Button>
+        </div>
+      </Fragment>
     );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
 export default EventDetailPage;
