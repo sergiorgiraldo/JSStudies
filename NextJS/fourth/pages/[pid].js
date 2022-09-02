97b80074c9d7a2e124c46db53776cb8f5358d84a
Sergio Giraldo <sergio@giraldo.com.br>
2022-09-02 14:44:32 +0200

    prettier:: by sergio giraldo @ 20220902T1444CEST, gpg signed

--- NextJS/fourth/pages/[pid].js
+++ NextJS/fourth/pages/[pid].js
@@ -3,40 +3,39 @@ import fs from "fs/promises"; //i can use the filesystem if I am accessing from 
 import path from "path";
 
 function ProductDetailPage(props) {
-  const { loadedProduct } = props;
+	const { loadedProduct } = props;
 
-  return (
-    <Fragment>
-      <h1>{loadedProduct.title}</h1>
-      <p>{loadedProduct.description}</p>
-    </Fragment>
-  );
+	if (!loadedProduct) {
+		return <p>Loading ...</p>;
+	}
+	return (
+		<Fragment>
+			<h1>{loadedProduct.title}</h1>
+			<p>{loadedProduct.description}</p>
+		</Fragment>
+	);
 }
 
 export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
+	const { params } = context;
+	const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data/dummy-backend.json"); //cwd() points to root folder
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+	const filePath = path.join(process.cwd(), "data/dummy-backend.json"); //cwd() points to root folder
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
 
-  const product = data.products.find((p) => p.id === productId);
+	const product = data.products.find((p) => p.id === productId);
 
-  return {
-    props: { loadedProduct: product },
-  };
+	return {
+		props: { loadedProduct: product },
+	};
 }
 
 export async function getStaticPaths() {
-    return {
-        paths:[
-            {params: {pid:"p1"}},
-            {params: {pid:"p2"}},
-            {params: {pid:"p3"}},
-        ],
-        fallback: false
-    }
+	return {
+		paths: [{ params: { pid: "p1" } }],
+		fallback: true,
+	};
 }
 
 export default ProductDetailPage;
