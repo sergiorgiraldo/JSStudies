91d9225ef6fc490b3b0894af9b57247701e82094
Sergio Giraldo <sergio@giraldo.com.br>
2022-09-07 17:31:34 +0200

    pretty:: by sergio giraldo @ 20220907T1731CEST, gpg signed

--- NextJS/fourth/pages/last-sales.js
+++ NextJS/fourth/pages/last-sales.js
@@ -1,44 +1,63 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-function LastSalesPage(props){
-    const [sales, setSales] = useState(props.sales);
+function LastSalesPage(props) {
+	const [sales, setSales] = useState(props.sales);
 
-    const {data, error} = 
-        useSWR("https://nextjs-course-d43b3-default-rtdb.europe-west1.firebasedatabase.app/sales.json", 
-        (url) => fetch(url).then(res => res.json()));
+	const { data, error } = useSWR(
+		"https://nextjs-course-d43b3-default-rtdb.europe-west1.firebasedatabase.app/sales.json",
+		(url) => fetch(url).then((res) => res.json())
+	);
 
-    useEffect(()=>{
-        if (data){
-            const transformedSales = [];
-            for (const key in data){
-                transformedSales.push({id:key, username:data[key].username, volume:data[key].volume});
-            }
-            setSales(transformedSales);
-        }
-        }, [data]);
+	useEffect(() => {
+		if (data) {
+			const transformedSales = [];
+			for (const key in data) {
+				transformedSales.push({
+					id: key,
+					username: data[key].username,
+					volume: data[key].volume,
+				});
+			}
+			setSales(transformedSales);
+		}
+	}, [data]);
 
-    if (error){
-        return <p>Something bad happened .... {error}</p>;
-    }
+	if (error) {
+		return <p>Something bad happened .... {error}</p>;
+	}
 
-    if (!data && !sales){
-        return <p>Loading ....</p>;
-    }
+	if (!data && !sales) {
+		return <p>Loading ....</p>;
+	}
 
-    return <ul>{sales.map(s=><li key={s.id}>{s.username} bought {s.volume}</li>)}</ul>;
+	return (
+		<ul>
+			{sales.map((s) => (
+				<li key={s.id}>
+					{s.username} bought {s.volume}
+				</li>
+			))}
+		</ul>
+	);
 }
 
-export async function getStaticProps(){
-    const response = await fetch("https://nextjs-course-d43b3-default-rtdb.europe-west1.firebasedatabase.app/sales.json");
-    const data = await response.json();
-    
-    const transformedSales = [];
-    for (const key in data){
-        transformedSales.push({id:key, username:data[key].username, volume:data[key].volume});
-    }
+export async function getStaticProps() {
+	const response = await fetch(
+		"https://nextjs-course-d43b3-default-rtdb.europe-west1.firebasedatabase.app/sales.json"
+	);
+	const data = await response.json();
 
-    return {props:{sales: transformedSales},revalidate:10};
+	const transformedSales = [];
+	for (const key in data) {
+		transformedSales.push({
+			id: key,
+			username: data[key].username,
+			volume: data[key].volume,
+		});
+	}
+
+	return { props: { sales: transformedSales }, revalidate: 10 };
 }
 
 export default LastSalesPage;
--- NextJS/fourth/pages/user-profile.js
+++ NextJS/fourth/pages/user-profile.js
@@ -1,14 +1,15 @@
-function UserProfilePage(props){
-    return <h1>{props.userName}</h1>
+function UserProfilePage(props) {
+	return <h1>{props.userName}</h1>;
 }
 
 export default UserProfilePage;
 
-export async function getServerSideProps(context){ //this runs on every access to the page
-    const {params, req, res} = context;
-    console.log("running on server");
-    
-    return{
-        props:{userName:"Sergio Rodrigues Giraldo"}
-    };
+export async function getServerSideProps(context) {
+	//this runs on every access to the page
+	const { params, req, res } = context;
+	console.log("running on server");
+
+	return {
+		props: { userName: "Sergio Rodrigues Giraldo" },
+	};
 }
