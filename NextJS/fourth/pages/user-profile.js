e99925f8cb2cbffa7641ac6ba18bd93ae2e81d56
Sergio Giraldo <sergio@giraldo.com.br>
2022-09-07 16:45:01 +0200

    fetching data on the server and in the client:: by sergio giraldo @ 20220907T1645CEST, gpg signed

---
+++ NextJS/fourth/pages/user-profile.js
@@ -0,0 +1,14 @@
+function UserProfilePage(props){
+    return <h1>{props.userName}</h1>
+}
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context){ //this runs on every access to the page
+    const {params, req, res} = context;
+    console.log("running on server");
+    
+    return{
+        props:{userName:"Sergio Rodrigues Giraldo"}
+    };
+}
