5cb2360f25177dd24296c156b95ea34e50395cce
Sergio Giraldo <sergio@giraldo.com.br>
2022-09-10 18:40:35 +0200

    getting events from firebase:: by sergio giraldo @ 20220910T1840CEST, gpg signed

--- NextJS/fifth/pages/events/[eventId].js
+++ NextJS/fifth/pages/events/[eventId].js
@@ -1,40 +1,59 @@
-import { Fragment } from "react";
-import { useRouter } from "next/router";
+import { Fragment } from 'react';
 
-import { getEventById } from "../../dummy-data";
-import EventSummary from "../../components/event-detail/event-summary";
-import EventLogistics from "../../components/event-detail/event-logistics";
-import EventContent from "../../components/event-detail/event-content";
-import ErrorAlert from "../../components/ui/error-alert";
+import { getEventById, getAllEvents } from '../../helpers/api-utils';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
-function EventDetailPage() {
-	const router = useRouter();
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
-	const eventId = router.query.eventId;
-	const event = getEventById(eventId);
+  if (!event) {
+    return (
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
 
-	if (!event) {
-		return (
-			<ErrorAlert>
-				<p>No event found!</p>
-			</ErrorAlert>
-		);
-	}
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
 
-	return (
-		<Fragment>
-			<EventSummary title={event.title} />
-			<EventLogistics
-				date={event.date}
-				address={event.location}
-				image={event.image}
-				imageAlt={event.title}
-			/>
-			<EventContent>
-				<p>{event.description}</p>
-			</EventContent>
-		</Fragment>
-	);
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event
+    },
+    revalidate: 30
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map(event => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: false
+  };
 }
 
 export default EventDetailPage;
