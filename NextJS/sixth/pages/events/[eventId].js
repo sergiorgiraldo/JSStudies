d40831c5f84922264f26f47883a0cefb0f928f12
Sergio Giraldo <sergio@giraldo.com.br>
2022-09-18 11:51:29 +0200

    head:: by sergio giraldo @ 20220918T1151CEST, gpg signed

--- NextJS/sixth/pages/events/[eventId].js
+++ NextJS/sixth/pages/events/[eventId].js
@@ -1,10 +1,9 @@
 import { Fragment } from "react";
-
+import Head from "next/head";
 import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
-import ErrorAlert from "../../components/ui/error-alert";
 
 function EventDetailPage(props) {
 	const event = props.selectedEvent;
@@ -19,6 +18,9 @@ function EventDetailPage(props) {
 
 	return (
 		<Fragment>
+			<Head>
+				<title>{event.title}</title>
+			</Head>
 			<EventSummary title={event.title} />
 			<EventLogistics
 				date={event.date}
@@ -43,6 +45,7 @@ export async function getStaticProps(context) {
 			selectedEvent: event,
 		},
 		revalidate: 30,
+		notFound: !event
 	};
 }
 
@@ -53,7 +56,7 @@ export async function getStaticPaths() {
 
 	return {
 		paths: paths,
-		fallback: "blocking",
+		fallback: true,
 	};
 }
 
