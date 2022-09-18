c907da083ad15651c8db48f89e43b898ac5b6000
Sergio Giraldo <sergio@giraldo.com.br>
2022-09-18 17:26:39 +0200

    putting 'global' head tags into _app.js:: by sergio giraldo @ 20220918T1726CEST, gpg signed

--- NextJS/sixth/pages/_app.js
+++ NextJS/sixth/pages/_app.js
@@ -1,9 +1,13 @@
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
+			<Head>
+				<link rel="author" content="Sergio" />
+			</Head>			
 			<Component {...pageProps} />
 		</Layout>
 	);
