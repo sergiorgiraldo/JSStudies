9f85299b56f4e6145d7dcfab15663d0d80fd382f
Sergio Giraldo <sergio@giraldo.com.br>
2022-09-18 11:51:29 +0200

    head:: by sergio giraldo @ 20220918T1151CEST, gpg signed

--- NextJS/sixth/pages/events/[...slug].js
+++ NextJS/sixth/pages/events/[...slug].js
@@ -1,14 +1,13 @@
 import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
-
-import { getFilteredEvents } from "../../helpers/api-util";
+import Head from "next/head";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 
-function FilteredEventsPage(props) {
+function FilteredEventsPage() {
 	const [loadedEvents, setLoadedEvents] = useState();
 	const router = useRouter();
 
@@ -33,15 +32,29 @@ function FilteredEventsPage(props) {
 		}
 	}, [data]);
 
+	let pageHeadData = (
+		<Head>
+			<link rel="author">Sergio</link>
+			<meta name="description" content="All filtered events"></meta>
+			<title>Filtered Events</title>
+		</Head>);
+
 	if (!loadedEvents) {
 		return <p className="center">Loading...</p>;
 	}
-
+	
 	const filteredYear = filterData[0];
 	const filteredMonth = filterData[1];
-
+	
 	const numYear = +filteredYear;
 	const numMonth = +filteredMonth;
+	
+	pageHeadData = (
+		<Head>
+			<link rel="author">Sergio</link>
+			<meta name="description" content={`Events from ${numMonth}/${numYear}`}></meta>
+			<title>Filtered Events!!!</title>
+		</Head>);
 
 	if (
 		isNaN(numYear) ||
@@ -54,6 +67,7 @@ function FilteredEventsPage(props) {
 	) {
 		return (
 			<Fragment>
+				{pageHeadData}
 				<ErrorAlert>
 					<p>Invalid filter. Please adjust your values!</p>
 				</ErrorAlert>
@@ -75,6 +89,7 @@ function FilteredEventsPage(props) {
 	if (!filteredEvents || filteredEvents.length === 0) {
 		return (
 			<Fragment>
+				{pageHeadData}
 				<ErrorAlert>
 					<p>No events found for the chosen filter!</p>
 				</ErrorAlert>
@@ -89,54 +104,11 @@ function FilteredEventsPage(props) {
 
 	return (
 		<Fragment>
+			{pageHeadData}
 			<ResultsTitle date={date} />
 			<EventList items={filteredEvents} />
 		</Fragment>
 	);
 }
-
-// export async function getServerSideProps(context) {
-//   const { params } = context;
-
-//   const filterData = params.slug;
-
-//   const filteredYear = filterData[0];
-//   const filteredMonth = filterData[1];
-
-//   const numYear = +filteredYear;
-//   const numMonth = +filteredMonth;
-
-//   if (
-//     isNaN(numYear) ||
-//     isNaN(numMonth) ||
-//     numYear > 2030 ||
-//     numYear < 2021 ||
-//     numMonth < 1 ||
-//     numMonth > 12
-//   ) {
-//     return {
-//       props: { hasError: true },
-//       // notFound: true,
-//       // redirect: {
-//       //   destination: '/error'
-//       // }
-//     };
-//   }
-
-//   const filteredEvents = await getFilteredEvents({
-//     year: numYear,
-//     month: numMonth,
-//   });
-
-//   return {
-//     props: {
-//       events: filteredEvents,
-//       date: {
-//         year: numYear,
-//         month: numMonth,
-//       },
-//     },
-//   };
-// }
 
 export default FilteredEventsPage;
--- NextJS/sixth/pages/index.js
+++ NextJS/sixth/pages/index.js
@@ -1,9 +1,14 @@
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 function HomePage(props) {
 	return (
 		<div>
+			<Head>
+				<title>Events!!!</title>
+				<meta name="description" content="Find nice events"></meta>
+			</Head>
 			<EventList items={props.events} />
 		</div>
 	);
