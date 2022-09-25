213c8c2a3b667675b43efa456bbf37822233a145
Sergio Giraldo <sergio@giraldo.com.br>
2022-09-25 16:05:21 +0200

    post and get routes:: by sergio giraldo @ 20220925T1605CEST, gpg signed

---
+++ NextJS/seventh/pages/feedback/index.js
@@ -0,0 +1,28 @@
+import { Fragment } from 'react';
+
+import { getAllFeedback } from '../api/feedback/index';
+
+function FeedbackPage(props){
+    return (
+        <Fragment>
+          <ul>
+            {props.feedbackItems.map((item) => (
+              <li key={item.id}>
+                {item.text}
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      );
+}
+
+export async function getStaticProps() {
+    const data = getAllFeedback();
+    return {
+      props: {
+        feedbackItems: data,
+      },
+    };
+}
+
+export default FeedbackPage;
