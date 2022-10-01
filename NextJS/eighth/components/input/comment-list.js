bad2c823eaac864caf9ef81940dfe66475730f27
Sergio Giraldo <sergio@giraldo.com.br>
2022-10-01 17:39:04 +0200

    comments ok, insert & retrieve:: by sergio giraldo @ 20221001T1739CEST, gpg signed

--- NextJS/eighth/components/input/comment-list.js
+++ NextJS/eighth/components/input/comment-list.js
@@ -1,21 +1,35 @@
 import classes from "./comment-list.module.css";
+import { useState, useEffect, Fragment } from "react";
+import useSWR from "swr";
 
-function CommentList() {
+function CommentList(props) {
+	const { eventId } = props;
+	const { data } = useSWR(`/api/comments/${eventId}`);
+	const [comments, setComments] = useState([]);
+
+	useEffect(() => {
+		if (data) {
+			setComments(data.allComments);
+		}
+	}, [data]);
+
+	if (!comments) {
+		return (
+			<Fragment>
+				<p className="center">Loading...</p>
+			</Fragment>
+		);
+	}	
+
 	return (
 		<ul className={classes.comments}>
-			{/* Render list of comments - fetched from API */}
-			<li>
-				<p>My comment is amazing!</p>
-				<div>
-					By <address>Maximilian</address>
-				</div>
-			</li>
-			<li>
-				<p>My comment is amazing!</p>
-				<div>
-					By <address>Maximilian</address>
-				</div>
-			</li>
+			{comments.map((c) => (
+				<li key={c.id}>
+					Name:: {c.name} <br/>
+					Email:: {c.email} <br/>
+					Comment:: {c.comment}
+				</li>
+			))}
 		</ul>
 	);
 }
