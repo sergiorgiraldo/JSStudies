aba4ed8285e6ce5b0ce8cb7504cde387701389fb
Sergio Giraldo <sergio@giraldo.com.br>
2022-10-04 11:39:08 +0200

    now this works, it was about yarn bin on the path:: by sergio giraldo @ 20221004T1139CEST, gpg signed

---
+++ mycli-with-gluegun/src/commands/foo.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: 'foo',
+  alias: 'f',
+  run: async function (toolbox) {
+    // gluegun provides all these features and more!
+    const { system, print, filesystem, strings } = toolbox
+
+    // ...and be the CLI you wish to see in the world
+    const awesome = strings.trim(await system.run('whoami'))
+    const moreAwesome = strings.kebabCase(`${awesome} and a keyboard`)
+    const contents = `🚨 Warning! ${moreAwesome} coming thru! 🚨`
+    const home = process.env['HOME']
+    filesystem.write(`${home}/realtalk.json`, { contents })
+
+    print.info(`${print.checkmark} Citius`)
+    print.warning(`${print.checkmark} Altius`)
+    print.success(`${print.checkmark} Fortius`)
+  },
+}
