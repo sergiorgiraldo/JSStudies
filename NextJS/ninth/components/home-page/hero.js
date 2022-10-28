11565e0bc03ea33efe4725a3f826880cf6106017
Sergio Giraldo <sergio@giraldo.com.br>
2022-10-28 20:49:58 +0200

    next js, ninth project, starting page already working with dummy data:: by sergio giraldo @ 20221028T2049CEST, gpg signed

---
+++ NextJS/ninth/components/home-page/hero.js
@@ -0,0 +1,23 @@
+import classes from "./hero.module.css";
+import Image from "next/image";
+
+function Hero() {
+	return (
+		<section className={classes.hero}>
+			<div className={classes.image}>
+            <Image
+                src="/images/site/srg.jpg"
+                alt="An image showing Sergio"
+                width={300}
+                height={300}
+            />
+			</div>
+			<h1>Hi, I'm Sergio</h1>
+			<p>
+				I blog about whatever I want to. Husband & Father first, geek, physicist, programmer, super heroes addicted.
+			</p>
+		</section>
+	);
+}
+
+export default Hero;
