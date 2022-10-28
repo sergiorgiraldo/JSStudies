1ec8e932127b4029f6f273b35af52ff576fb1f10
Sergio Giraldo <sergio@giraldo.com.br>
2022-10-28 20:49:58 +0200

    next js, ninth project, starting page already working with dummy data:: by sergio giraldo @ 20221028T2049CEST, gpg signed

--- NextJS/ninth/pages/index.js
+++ NextJS/ninth/pages/index.js
@@ -1,5 +1,21 @@
-function HomePage(){
+import { Fragment } from "react";
+import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
 
+const DUMMY = [
+    {title:"foo", image:"i.png", excerpt:"foo foo foo foo foo foo", date:"2022-10-28", slug:"1"},
+    {title:"bar", image:"i.png", excerpt:"bar bar bar bar bar bar", date:"2022-10-28", slug:"2"},
+    {title:"baz", image:"i.png", excerpt:"baz baz baz baz baz baz", date:"2022-10-28", slug:"3"},
+
+];
+
+function HomePage(){
+    return (
+        <Fragment>
+            <Hero/>
+            <FeaturedPosts posts={DUMMY}/>
+        </Fragment>    
+    );
 }
 
 export default HomePage;
