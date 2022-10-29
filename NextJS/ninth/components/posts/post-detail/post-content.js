185b69656340e4376a9587b7268f1309523f6227
Sergio Giraldo <sergio@giraldo.com.br>
2022-10-29 17:17:03 +0200

    all pages for posts, with Markdown capabilitie:: by sergio giraldo @ 20221029T1717CEST, gpg signed

---
+++ NextJS/ninth/components/posts/post-detail/post-content.js
@@ -0,0 +1,18 @@
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+import ReactMarkdown from "react-markdown";
+
+function PostContent(props) {
+	const { post } = props;
+
+	const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+	return (
+		<article className={classes.content}>
+			<PostHeader title={post.title} image={imagePath} />
+			<ReactMarkdown>{post.content}</ReactMarkdown>
+		</article>
+	);
+}
+
+export default PostContent;
