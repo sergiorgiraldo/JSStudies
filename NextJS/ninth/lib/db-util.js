5f590c3fca8b865e6164e29648c65c3d6e9a6af8
Sergio Giraldo <sergio@giraldo.com.br>
2022-11-02 08:31:41 +0100

    saving contact form to mongodb using api call:: by sergio giraldo @ 20221102T0831CET, gpg signed

--- NextJS/ninth/lib/db-util.js
+++ NextJS/ninth/lib/db-util.js
@@ -4,26 +4,13 @@ export async function connectDatabase() {
 	const client = await MongoClient.connect(
 		"mongodb+srv://sa:sa@cluster0.nnllzq2.mongodb.net/?retryWrites=true&w=majority"
 	);
-
 	return client;
 }
 
 export async function insertDocument(client, collection, document) {
-	const db = client.db("test");
-
+	const db = client.db("blog");
 	const result = await db.collection(collection).insertOne(document);
 
 	return result;
 }
-
-export async function getAllDocuments(client, collection, sort, filter = {}) {
-	const db = client.db("test");
-
-	const documents = await db
-		.collection(collection)
-		.find(filter)
-		.sort(sort)
-		.toArray();
-
-	return documents;
-}
+	
