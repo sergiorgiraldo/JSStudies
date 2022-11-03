8c7789ac01d3e5a7db69941dd25e13ef61de6c8d
Sergio Giraldo <sergio@giraldo.com.br>
2022-11-03 16:14:45 +0100

    environment variables:: by sergio giraldo @ 20221103T1614CET, gpg signed

---
+++ NextJS/tenth/pages/posts/[slug].js
@@ -0,0 +1,38 @@
+import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
+
+function PostDetailPage(props) {
+	const { whichPost } = props;
+
+	if (!whichPost) {
+		return <p>Loading ...</p>;
+	}
+
+	return <PostContent post={whichPost} />;
+}
+
+export async function getStaticProps(context) {
+	const { params } = context;
+	const { slug } = params;
+	const post = getPostData(`${slug}.md`);
+
+	return {
+		props: { whichPost: post },
+		revalidate: 600
+	};
+}
+
+export async function getStaticPaths() {
+	const postFilenames = getPostsFiles();
+
+	const slugs = postFilenames.map((fileName) =>
+		fileName.replace(/\.md$/, "")
+	);
+
+	return {
+		paths: slugs.map((slug) => ({ params: { slug: slug } })),
+		fallback: false
+	};
+}
+
+export default PostDetailPage;
