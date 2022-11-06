334e8d475e28d0a665033f744f9e3c0ce6e63736
Sergio Giraldo <sergio@giraldo.com.br>
2022-11-06 16:00:21 +0100

    final version with auth, guarding pages and api routes:: by sergio giraldo @ 20221106T1600CET, gpg signed

--- NextJS/eleventh/components/layout/main-navigation.js
+++ NextJS/eleventh/components/layout/main-navigation.js
@@ -1,8 +1,14 @@
 import Link from "next/link";
-
+import { useSession, signOut } from "next-auth/client";
 import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
+	const [session, loading] = useSession();
+
+	function logoutHandler() {
+		signOut();
+	}
+
 	return (
 		<header className={classes.header}>
 			<Link href="/">
@@ -12,15 +18,22 @@ function MainNavigation() {
 			</Link>
 			<nav>
 				<ul>
-					<li>
-						<Link href="/auth">Login</Link>
-					</li>
-					<li>
-						<Link href="/profile">Profile</Link>
-					</li>
-					<li>
-						<button>Logout</button>
-					</li>
+					{!session && !loading && (
+						<li>
+							<Link href="/auth">Login</Link>
+						</li>
+					)}
+					{!session && loading && <li>Loading ...</li>}
+					{session && (
+						<li>
+							<Link href="/profile">Profile</Link>
+						</li>
+					)}
+					{session && (
+						<li>
+							<button onClick={logoutHandler}>Logout</button>
+						</li>
+					)}
 				</ul>
 			</nav>
 		</header>
