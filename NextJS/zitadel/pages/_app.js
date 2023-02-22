80efb19a4d2740d48472c0e9188c7e282ee4c1cb
Sergio Giraldo <sergio@giraldo.com.br>
2023-02-22 17:03:51 +0100

    fixing bug, profile data not being returned:: by sergio giraldo @ 20230222T1703CET, gpg signed

--- NextJS/zitadel/pages/_app.js
+++ NextJS/zitadel/pages/_app.js
@@ -1,13 +1,13 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 import { SessionProvider } from "next-auth/react";
 
 function App({ Component, pageProps }) {
-return (
-  <SessionProvider session={pageProps.session}>
-    <Component {...pageProps} />
-  </SessionProvider>
-);
+	return (
+		<SessionProvider session={pageProps.session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
 
 export default App;
