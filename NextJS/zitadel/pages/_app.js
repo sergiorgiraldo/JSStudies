e20045cb491f6d6683764f0c279382e03c7503aa
Sergio Giraldo <sergio@giraldo.com.br>
2023-02-22 16:40:53 +0100

    integration with Zitadel authentication- nextjs:: by sergio giraldo @ 20230222T1640CET, gpg signed

---
+++ NextJS/zitadel/pages/_app.js
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+
+import { SessionProvider } from "next-auth/react";
+
+function App({ Component, pageProps }) {
+return (
+  <SessionProvider session={pageProps.session}>
+    <Component {...pageProps} />
+  </SessionProvider>
+);
+}
+
+export default App;
---
+++ NextJS/zitadel/pages/index.js
@@ -0,0 +1,20 @@
+import { signIn, signOut, useSession } from "next-auth/react"
+
+const callbackUrl = 'http://localhost:3000/profile'
+
+export default function Page() {
+  const { data: session } = useSession();
+
+ return <div>
+  {!session && <>
+    Not signed in <br />
+    <button onClick={() => signIn('zitadel', { callbackUrl })}>
+      Sign in
+    </button>
+  </>}
+  {session && <>
+    Signed in as {session.user.email} <br />
+    <button onClick={() => signOut()}>Sign out</button>
+  </>}
+ </div>
+}
