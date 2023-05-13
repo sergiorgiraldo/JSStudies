a2b2d5f28869af521246d22a223fe29119014105
Sergio Giraldo <sergio@giraldo.com.br>
2023-05-13 15:47:41 +0200

    more cypress tests:: by sergio giraldo @ 20230513T1547CEST, gpg signed

--- ngx-cypress-test/cypress/e2e/firstTest.spec.js
+++ ngx-cypress-test/cypress/e2e/firstTest.spec.js
@@ -131,7 +131,7 @@ describe('Our first suite', () => {
         //     .should('contain', 'checked');
     })
 
-    it.only('assert property', () => {
+    it('assert property', () => {
         function selectDayFromCurrent(day){
             let date = new Date();
             date.setDate(date.getDate() + day);
@@ -155,10 +155,46 @@ describe('Our first suite', () => {
         cy.contains('Datepicker').click();
 
         cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
-            cy.wrap(input).click();
+            cy.wrap(input).click(); //the input is a jquery element, not the actual element. Wrap makes the element as the context, then i can click
             let dateAssert = selectDayFromCurrent(2);
-            cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert);
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert); //prop is the object name for the properties
             cy.wrap(input).should('have.value', dateAssert);
         })
+    })
+
+    it.only('radio button', () => {
+        cy.visit('/');
+        cy.contains('Forms').click();
+        cy.contains('Form Layouts').click();
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+            cy.wrap(radioButtons)
+                .first()
+                .check({force: true})
+                .should('be.checked');
+
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({force: true});
+                
+            cy.wrap(radioButtons)
+                .eq(0)
+                .should('not.be.checked');
+                
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled') ;
+        });
+    })
+
+    it('check boxes', () => {
+        cy.visit('/');
+        cy.contains('Modal & Overlays').click();
+        cy.contains('Toastr').click();
+
+        //cy.get('[type="checkbox"]').check({force:true})
+        cy.get('[type="checkbox"]').eq(0).click({force:true});
+        cy.get('[type="checkbox"]').eq(1).check({force:true});
+
     })
 })
