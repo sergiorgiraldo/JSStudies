5bb90d856967fb676f6f57ebe464b2581cdcb532
Sergio Giraldo <sergio@giraldo.com.br>
2023-05-26 21:54:54 +0200

    tooltips, dialogos and asssertions in cypress:: by sergio giraldo @ 20230526T2154CEST, gpg signed

--- ngx-cypress-test/cypress/e2e/firstTest.spec.js
+++ ngx-cypress-test/cypress/e2e/firstTest.spec.js
@@ -334,7 +334,7 @@ describe("Our first suite", () => {
 		});
 	});
 
-	it.only("tooltip", () => {
+	it("tooltip", () => {
 		cy.visit("/");
 		cy.contains("Modal & Overlays").click();
 		cy.contains("Tooltip").click();
@@ -342,4 +342,29 @@ describe("Our first suite", () => {
 		cy.contains("nb-card", "Colored Tooltips").contains("Default").click();
 		cy.get("nb-tooltip").should("contain", "This is a tooltip");
 	});
+
+	it.only('dialog box', () => {
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+        
+        //1
+        // cy.get('tbody tr').first().find('.nb-trash').click()
+        // cy.on('window:confirm', (confirm) => {
+        //     expect(confirm).to.equal('Are you sure you want to delete?')
+        // }) <- this is not a good way because if there is no confirm windows, the on event wont fire and the assert will not be called (and also not an error)
+
+        //2
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+        cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+        })
+
+        //3
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', () => false)
+
+
+    })
 });
