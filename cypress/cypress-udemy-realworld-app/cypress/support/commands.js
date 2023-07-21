218dba7d94f14ee27d2998d5c7346c37dd508175
Sergio Giraldo <sergio@giraldo.com.br>
2023-07-21 14:06:43 +0200

    folder restructure:: by sergio giraldo @ 20230721T1406CEST, gpg signed

---
+++ cypress/cypress-udemy-realworld-app/cypress/support/commands.js
@@ -0,0 +1,48 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("loginToApplication", () => {
+
+
+    const userCredentials = {
+        "user": {
+            "email": Cypress.env("username"),
+            "password": Cypress.env("password")
+        }
+    }
+
+    cy.request('POST', Cypress.env('apiUrl')+'/api/users/login', userCredentials)
+        .its('body').then( body => {
+            const token = body.user.token
+            cy.wrap(token).as('token')
+            cy.visit('/', {
+                onBeforeLoad (win){
+                    win.localStorage.setItem('jwtToken', token)
+                }
+            })
+        });
+    
+});
