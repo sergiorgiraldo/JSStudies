c9f3406b68acf76691a2dbbbade06ea8bd8481c9
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-03 08:03:20 +0200

    feat(svelte): first app in svelte with some use, create an alias in SimpleLogin ::by sergio giraldo @ 20231003T0803CEST, gpg signed

---
+++ simple-login-create-alias/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+
+<script>
+    import { getRandomName } from "$lib/name-creation";
+    import { postSimpleLogin } from "$lib/create-alias"
+
+    let newName = getRandomName();
+    $: alias = newName;
+
+    function updateName(){
+        newName = getRandomName();
+    }
+
+    function createAlias(){
+        postSimpleLogin(alias);
+    }
+</script>
+<main class="container">
+    <h1>SimpleLogin Create Alias</h1>
+    <section id="form">
+            <h3>Alias</h3> 
+            <p>
+                {alias}
+            </p>
+
+            <button on:click={updateName}>I didnt like this one</button>
+            <br />
+            <button on:click={createAlias}>Seems fine</button>
+    </section>
+</main>
