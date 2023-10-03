6ed1a836a50aa631eadb5cacf4fc1bfea4de0178
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-03 15:08:59 +0200

    feat(svelte): udemy course, basic syntax ::by sergio giraldo @ 20231003T1508CEST, gpg signed

---
+++ svelte/udemy/01-first-app/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	import ContactCard from "./ContactCard.svelte";
+
+	export let name;
+	let age = 10;
+
+	$: uppercaseName = name.toUpperCase();
+
+	$: console.log(name);
+
+	$: if (name == "Sergio"){
+		age = 51;
+	}
+
+	function incrementAge(){
+		age += 1;
+	}
+
+	function changeName(){
+		name = "Sergio";
+	}
+
+	function supplyName(event){
+		name = event.target.value;
+	}
+
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {uppercaseName}, age is {age}!</h1>
+<button on:click={incrementAge}>Change age</button>
+<!-- <button on:click={changeName}>Change name</button> -->
+<br/>
+<!-- <input type="text" value={name} on:input={supplyName}/> -->
+<input type="text" bind:value={name} />
+<br/>
+<ContactCard />
