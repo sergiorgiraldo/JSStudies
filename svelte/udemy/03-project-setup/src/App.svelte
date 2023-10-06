bd516725f5321701754f37bdfe38bf8688fa6f47
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-06 15:51:59 +0200

    feat(svelte): meetup application, second part with meetup items. also converted to typescript ::by sergio giraldo @ 20231006T1551CEST, gpg signed

--- svelte/udemy/03-project-setup/src/App.svelte
+++ svelte/udemy/03-project-setup/src/App.svelte
@@ -1,32 +1,44 @@
+<script lang="ts">
+	import Header from "./UI/Header.svelte";
+	import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+    import IMeetup from "../interfaces/Meetup";
 
-<script>
-    import Header from "./UI/Header.svelte";
-
-    const mmetups = [
-        {
-            id: "m1",
-            title: "Coding",
-            subtitle: "Learn to code",
-            description: "Meet experts in development of svelte applications",
-            imageUrl: "https://placehold.co/600x400/orange/white?text=Learn+To+Code",
-            address:"1st Avenue New York",
-            contactEmail: "john@example.com"
-        },
-        {
-            id: "m2",
-            title: "Coding",
-            subtitle: "Learn to code",
-            description: "Meet experts in development of svelte applications",
-            imageUrl: "https://placehold.co/600x400/orange/white?text=Learn+To+Code",
-            address:"1st Avenue New York",
-            contactEmail: "john@example.com"
-        },
-    ];
+	let meetups:IMeetup[] = [
+		{
+			id: "m1",
+			title: "Coding",
+			subtitle: "Learn to code",
+			description: "In aute ut officia magna aliqua esse laborum deserunt minim. Cupidatat ex ad aliquip est minim adipisicing laborum non.",
+			imageUrl:
+				"https://placehold.co/600x400/orange/white?text=Learn+To+Code",
+			address: "1st Avenue New York",
+			contactEmail: "john@example.com",
+			capacity: 30
+		},
+		{
+			id: "m2",
+			title: "Coding",
+			subtitle: "Learn to code",
+			description: "Pariatur officia adipisicing fugiat eiusmod nisi sit amet esse. Nisi sunt amet veniam excepteur sint ea officia elit nostrud.",
+			imageUrl:
+				"https://placehold.co/600x400/cyan/white?text=Learn+To+Code",
+			address: "2nd Avenue New York",
+			contactEmail: "jane@example.com",
+			capacity: 120
+		}
+	];
 </script>
 
+<header>
+	<Header />
+</header>
+
+<main>
+	<MeetupGrid meetups={meetups} />
+</main>
+
 <style>
+	main{
+		margin-top: 5rem;
+	}
 </style>
-
-<Header />
-
-
