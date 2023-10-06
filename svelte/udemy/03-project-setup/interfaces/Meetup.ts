b4449e93216f9ae9a249d00259575447b031f8f0
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-06 15:51:59 +0200

    feat(svelte): meetup application, second part with meetup items. also converted to typescript ::by sergio giraldo @ 20231006T1551CEST, gpg signed

---
+++ svelte/udemy/03-project-setup/interfaces/Meetup.ts
@@ -0,0 +1,10 @@
+export default interface IMeetup{
+    id: string;
+    title: string;
+    subtitle: string;
+    imageUrl: string;
+    description: string;
+    address: string;
+    contactEmail: string;
+    capacity: number;
+}
---
+++ svelte/udemy/03-project-setup/src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import MeetupItem from "./MeetupItem.svelte";
+    import IMeetup from "../../interfaces/Meetup";
+
+	export let meetups: IMeetup[];
+</script>
+
+<section id="meetups">
+	{#each meetups as meetup}
+		<MeetupItem
+			title={meetup.title}
+			subtitle={meetup.subtitle}
+			description={meetup.description}
+			imageUrl={meetup.imageUrl}
+			address={meetup.address}
+            capacity={meetup.capacity}
+			email={meetup.contactEmail} />
+	{/each}
+</section>
+
+<style>
+	section {
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-gap: 1rem;
+	}
+
+	@media (min-width: 768px) {
+		section {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+</style>
