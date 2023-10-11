88458264c4e4d4f2606498bcd24ef29c8c60f684
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-11 21:44:42 +0200

    feat(svelte): udemy course, third module, custom component+custom event ::by sergio giraldo @ 20231011T2147CEST, gpg signed

---
+++ svelte/udemy/04-project-setup/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Product from "./Product.svelte";
+</script>
+
+<Product
+	ProductTitle="My Product"
+	on:add-cart={(e) => alert("Added to cart:" + e.detail)}
+	on:delete-cart={() => alert("Deleted from cart")} />
---
+++ svelte/udemy/04-project-setup/src/Product.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { createEventDispatcher } from "svelte";
+
+	export let productTitle = "";
+
+	const dispatch = createEventDispatcher();
+
+	function addtoCart() {
+		dispatch("add-cart", "p1");
+	}
+</script>
+
+<article>
+	<h1>{productTitle}</h1>
+	<button on:click={addtoCart}>Add to Cart</button>
+	<button on:click={() => dispatch("delete-cart")}>Delete from Cart</button>
+</article>
