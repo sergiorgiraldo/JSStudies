431fe80e037d8e140611b058238bf365d77a34a9
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-11 16:09:17 +0200

    feat(svelte): meetup application, finished module with forms and custom components ::by sergio giraldo @ 20231011T1609CEST, gpg signed

--- svelte/udemy/03-project-setup/src/App.svelte
+++ svelte/udemy/03-project-setup/src/App.svelte
@@ -2,6 +2,8 @@
 	import Header from "./UI/Header.svelte";
 	import MeetupGrid from "./Meetups/MeetupGrid.svelte";
     import IMeetup from "../interfaces/Meetup";
+	import TextInput from "./UI/TextInput.svelte";
+	import Button from "./UI/Button.svelte";
 
 	let title:string = "";
 	let subtitle:string = "";
@@ -9,7 +11,7 @@
 	let description:string = "";
 	let address:string = "";
 	let email:string = "";
-	let capacity:number = 0;
+	let capacity:string = "0";
 
 	let meetups:IMeetup[] = [
 		{
@@ -22,6 +24,17 @@
 			address: "1st Avenue New York",
 			contactEmail: "john@example.com",
 			capacity: 30
+		},
+		{
+			id: "m2",
+			title: "Coding",
+			subtitle: "Learn CSS",
+			description: "In aute ut officia magna aliqua esse laborum deserunt minim. Cupidatat ex ad aliquip est minim adipisicing laborum non.",
+			imageUrl:
+				"https://placehold.co/600x400/violet/black?text=Learn+CSS",
+			address: "1st Avenue Chicago",
+			contactEmail: "jane@example.com",
+			capacity: 45
 		}
 	];
 
@@ -34,7 +47,7 @@
 			"description": description,
 			"address": address,
 			"contactEmail": email,
-			"capacity": capacity
+			"capacity": parseInt(capacity, 10)
 		};
 		meetups = [...meetups, newMeetup];
 	}
@@ -46,42 +59,58 @@
 
 <main>
 	<form on:submit|preventDefault={addMeetup}>
-		<div class="form-control">
-			<label for="input_title">TITLE<label>:
-			<input type="text" id="title" bind:value={title} />
-		</div>
-			
-		<div class="form-control">
-			<label for="input_subtitle">SUBTITLE<label>:
-			<input type="text" id="subtitle" bind:value={subtitle} />
-		</div>
-			
-		<div class="form-control">
-			<label for="input_imageUrl">IMAGEURL<label>:
-			<input type="text" id="imageUrl"  bind:value={imageUrl} />
-		</div>
-			
-		<div class="form-control">
-			<label for="input_description">DESCRIPTION<label>:
-			<input type="text" id="description" bind:value={description} />
-		</div>
-			
-		<div class="form-control">
-			<label for="input_address">ADDRESS<label>:
-			<input type="text" id="address"  bind:value={address} />
-		</div>
-			
-		<div class="form-control">
-			<label for="input_contactEmail">CONTACTEMAIL<label>:
-			<input type="text" id="contactEmail" bind:value={email} />
-		</div>
-			
-		<div class="form-control">
-			<label for="input_capacity">CAPACITY<label>:
-			<input type="text" id="capacity"  bind:value={capacity} />
-		</div>   		  
-			
-		<button>Add Meetup</button>
+
+		<TextInput
+		id="title"
+		label="Title"
+		controlType="text"
+		value={title}
+		on:input={(event) => (title = event.target?event.target.value:"")} />
+
+		<TextInput
+		id="subtitle"
+		label="Subtitle"
+		controlType="text"
+		value={subtitle}
+		on:input={event => (subtitle = event.target?event.target.value:"")} />
+		
+		<TextInput
+		id="address"
+		label="Address"
+		controlType="text"
+		value={address}
+		on:input={event => (address = event.target?event.target.value:"")} />
+		
+		<TextInput
+		id="capacity"
+		label="Capacity"
+		controlType="number"
+		value={capacity}
+		on:input={event => (capacity = event.target?event.target.value:"")} />
+		
+		<TextInput
+		id="imageUrl"
+		label="Image URL"
+		controlType="text"
+		value={imageUrl}
+		on:input={event => (imageUrl = event.target?event.target.value:"")} />
+		
+		<TextInput
+		id="email"
+		label="E-Mail"
+		controlType="email"
+		value={email}
+		on:input={event => (email = event.target?event.target.value:"")} />
+		
+		<TextInput
+		id="description"
+		label="Description"
+		controlType="textarea"
+		rows={3}
+		value={description}
+		on:input={event => (description = event.target?event.target.value:"")} />
+					  
+		<Button type="button" caption="Add Meetup" />
 	</form>
 	<MeetupGrid meetups={meetups} />
 </main>
@@ -89,5 +118,11 @@
 <style>
 	main{
 		margin-top: 5rem;
+		background-color:whitesmoke;
+	}
+	form{
+		width: 30rem;
+		max-width: 90%;
+		margin:auto;
 	}
 </style>
---
+++ svelte/udemy/03-project-setup/src/UI/TextInput.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	export let controlType: string;
+	export let id: string;
+	export let label: string;
+	export let rows: number = 3;
+	export let value: string;
+</script>
+
+<div class="form-control">
+	<label for={id}>{label}</label>
+	{#if controlType === "textarea"}
+		<textarea {rows} {id} {value} on:input />
+	{:else}
+		<input type={controlType} {id} {value} on:input />
+	{/if}
+</div>
+
+<style>
+	input,
+	textarea {
+		display: block;
+		width: 100%;
+		font: inherit;
+		border: none;
+		border-bottom: 2px solid #ccc;
+		border-radius: 3px 3px 0 0;
+		background: white;
+		padding: 0.15rem 0.25rem;
+		transition: border-color 0.1s ease-out;
+	}
+
+	input:focus,
+	textarea:focus {
+		border-color: #e40763;
+		outline: none;
+	}
+
+	label {
+		display: block;
+		margin-bottom: 0.5rem;
+		width: 100%;
+	}
+
+	.form-control {
+		padding: 0.5rem 0;
+		width: 100%;
+		margin: 0.25rem 0;
+	}
+</style>
