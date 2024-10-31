a0f71b232440447685186d278619e88a62b0f9c6
Sergio Giraldo <sergio@giraldo.com.br>
2024-10-31 23:10:35 +0100

    feat: site in svelte made in copilot, it does not work lol ::by sergio giraldo @ 20241031T2310CET, gpg signed

--- svelte/squash-reservation/src/App.svelte
+++ svelte/squash-reservation/src/App.svelte
@@ -1,16 +1,18 @@
 <script>
-  import { Router, Route, Link } from "svelte-routing";
-  import DataEntry from "./DataEntry.svelte";
-  import Reservations from "./Reservations.svelte";
+	import { Router, Route, Link } from "svelte-routing";
+	import DataEntry from "./DataEntry.svelte";
+	import Reservations from "./Reservations.svelte";
 </script>
 
 <main>
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/reservations">Reservations</Link>
-  </nav>
-  <Router>
-    <Route path="/" component={DataEntry} />
-    <Route path="/reservations" component={Reservations} />
-  </Router>
+	<h1>Squash Reservations</h1>
+	<Router>
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="/reservations">Reservations</Link>
+		</nav>
+    <hr />
+		<Route path="/" component={DataEntry} />
+		<Route path="/reservations" component={Reservations} />
+	</Router>
 </main>
